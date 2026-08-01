ad8884de75e91e8df7001d6e1a3dfbd2c19c014d
HARIPRIYA M A <haripriyaajai99@gmail.com>
2026-08-01 11:22:51 +0000

    responsive

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function(e) {
-e.preventDefault();
+const roles = [
+"C++ Developer",
+"Software Engineer",
+"Python Developer",
+"Problem Solver"
+];
 
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-behavior: 'smooth'
-});
+let roleIndex = 0;
+let charIndex = 0;
 
-});
-});
+function typeEffect(){
+
+const currentRole = roles[roleIndex];
+
+document.getElementById("typing").textContent =
+currentRole.substring(0,charIndex);
+
+charIndex++;
+
+if(charIndex > currentRole.length){
+
+roleIndex++;
+
+charIndex = 0;
+
+if(roleIndex >= roles.length){
+roleIndex = 0;
+}
+
+setTimeout(typeEffect,1000);
+}
+else{
+setTimeout(typeEffect,100);
+}
+}
+
+typeEffect();
